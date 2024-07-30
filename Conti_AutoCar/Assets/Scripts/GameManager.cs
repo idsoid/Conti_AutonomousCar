d41319eb913037c54f1c5c5923971a8707d13dce
ContiAccount <nino2004sanchez@gmail.com>
2024-07-30 09:17:11 +0800

    wasd

--- Conti_AutoCar/Assets/Scripts/GameManager.cs
+++ Conti_AutoCar/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -33,6 +34,8 @@ public class GameManager : MonoBehaviour
     private bool gameOver = false;
     [SerializeField]
     private GameObject scoreboardCanvas;
+    [SerializeField]
+    private TextMeshPro scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -100,6 +103,7 @@ public class GameManager : MonoBehaviour
         if (gameOver)
         {
             scoreboardCanvas.SetActive(true);
+            scoreText.text = "Score: " + score;
         }
     }
     
