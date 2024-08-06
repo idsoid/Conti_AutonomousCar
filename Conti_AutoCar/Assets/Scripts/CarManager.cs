1ac4b329a322b2f34c8e4f0d0dbc5a8834302378
ContiAccount <nino2004sanchez@gmail.com>
2024-08-06 18:52:10 +0800

    music screen

--- Conti_AutoCar/Assets/Scripts/CarManager.cs
+++ Conti_AutoCar/Assets/Scripts/CarManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarManager : MonoBehaviour
 {
+    [SerializeField]
+    private MeshRenderer displayMeshRenderer, leftMeshRenderer, rightMeshRenderer;
+    [SerializeField]
+    private ToggleGroup themeChoice;
+    private int themeNumber;
     [SerializeField]
     private List<Material> screenMaterial;
     [SerializeField]
@@ -24,5 +30,33 @@ public class CarManager : MonoBehaviour
     public void ToggleGame()
     {
         gameHead.SetActive(!gameHead.activeSelf);
+    }
+    public void SaveTheme()
+    {
+        foreach (var toggle in themeChoice.ActiveToggles())
+        {
+            if (toggle.isOn)
+            {
+                switch (toggle.name)
+                {
+                    case "sand":
+                        themeNumber = 0;
+                        break;
+                    case "futuristic":
+                        themeNumber = 1;
+                        break;
+                    case "honeycomb":
+                        themeNumber = 2;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            }
+        }
+    }
+    public void SetTheme()
+    {
+        displayMeshRenderer.material = screenMaterial[themeNumber];
     }
 }
