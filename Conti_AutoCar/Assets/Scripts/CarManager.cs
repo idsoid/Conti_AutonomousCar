07e3360b1fb33b1c8a94ad52fb77db9a8b98fd55
ContiAccount <nino2004sanchez@gmail.com>
2024-08-07 14:36:44 +0800

    animation update

--- Conti_AutoCar/Assets/Scripts/CarManager.cs
+++ Conti_AutoCar/Assets/Scripts/CarManager.cs
@@ -14,6 +14,7 @@ public class CarManager : MonoBehaviour
     private List<Material> screenMaterial;
     [SerializeField]
     private GameObject gameHead;
+    public bool musicSelected = false;
 
     // Start is called before the first frame update
     void Start()
