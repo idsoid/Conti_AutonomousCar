04b71314919eaff6749a10c0239559e5e8ba999b
ContiAccount <nino2004sanchez@gmail.com>
2024-08-13 12:08:36 +0800

    yea

--- Conti_AutoCar/Assets/Scripts/PlayerManager.cs
+++ Conti_AutoCar/Assets/Scripts/PlayerManager.cs
@@ -11,13 +11,7 @@ public class PlayerManager : MonoBehaviour
     private Transform playerCamera, playerMaxHeight, playerMinHeight, playerMaxLength, playerMinLength;
     public bool playerSet = false;
     private int adjustCount = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
